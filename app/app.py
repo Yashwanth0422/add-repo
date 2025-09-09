98e58bb6e11eb59fa79dcb5c562382be0ffb2b50
Yashwanth0422 <yashuyashwanth95@gmail.com>
2025-09-09 15:57:26 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify
-
 app = Flask(__name__)
 
 @app.route('/healthz')
 def healthz():
-    return jsonify(status='ok'), 200
+    return jsonify(status="ok")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
