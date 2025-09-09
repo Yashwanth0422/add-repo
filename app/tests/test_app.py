84f1b7552f5f052fb80db6aa0208e6154a622c83
Yashwanth0422 <yashuyashwanth95@gmail.com>
2025-09-09 16:12:30 +0530

    Delete app directory

--- app/tests/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_healthz():
-    with app.test_client() as c:
-        rv = c.get('/healthz')
-        assert rv.status_code == 200
-        assert rv.json['status'] == 'ok'
