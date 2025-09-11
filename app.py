8de3720b77d6d50c080a1278a390e2bc4baa4376
Yashwanth0422 <yashuyashwanth95@gmail.com>
2025-09-11 12:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/healthz')
 def healthz():
-    return jsonify(status="ok")
+    return """
+    <html>
+      <head>
+        <title>CHOCHO</title>
+      </head>
+      <body>
+        Hi Iam CHOCHO 
+        <a href="http://www.youtube.com/@Chochothechowchow" target="_blank">
+          www.youtube.com/@Chochothechowchow
+        </a>
+      </body>
+    </html>
+    """, 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
+
